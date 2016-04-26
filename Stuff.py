edd5ca32375a32a82e172ce1aea8f1279465e75e
clarine kirui <clarine.kirui@gmail.com>
2016-04-26 14:54:47 +0300

    Assignment 1

---
+++ Stuff.py
@@ -0,0 +1,17 @@
+
+# print list(map(int, str(234567)))
+# print dict(1="boyr", 2="you")
+
+def add(a, b):
+	""" sometin cool
+	"""
+	# import pdb; pdb.set_trace()
+	# if type(a) == int and type(b) == int:
+	# 	print (a + b)
+	# else:
+	# 	print ("Invalid input")
+	print a + b
+
+add(1,5)
+add('1','5')
+add(1.1,5.6)
