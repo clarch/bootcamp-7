a7187c35111a546ed8e4742f00e98007768d7932
clarine kirui <clarine.kirui@gmail.com>
2016-04-27 19:41:27 +0300

    Assignement - formating and unpacking a list of tuples with different lenghts

--- Assignment/irregular_list_unpacking.py
+++ Assignment/irregular_list_unpacking.py
@@ -1,4 +1,9 @@
-find = {(60, 70, 90, 9)(4, 5),(20, 30, 50)}
+find = [(60, 70, 90, 9),(4, 5),(20, 30, 50)]
 
-nw_ls = [x[0] for x in find]
-nw_ls
+for f in find:
+	if len(f) == 2:
+		print "x: {}, y: {}".format(*f)
+	elif len(f) == 3:
+		print "x: {}, y: {}, z: {}".format(*f)
+	else:
+		print "Invalid Entry"
